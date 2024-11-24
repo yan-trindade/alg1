d1885e94fd03b44bca7ce1b79fad842d5af6ed38
Yan Trindade <67748194+yan-trindade@users.noreply.github.com>
2024-11-24 13:20:14 -0300

    Update set.h

--- projeto2/set.h
+++ projeto2/set.h
@@ -1,8 +1,9 @@
 #ifndef SET_H
     #define SET_H
 
-    #include<stdbool.h>
-    #include<stdlib.h>
+    #include <stdbool.h>
+    #include <stdlib.h>
+    #include <stdio.h>
     #include "avl.h"
     #include "rb.h"
 
