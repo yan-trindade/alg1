2845ae271ae7388ea1d9d155160724ba5cfbc6e1
Yan Trindade <67748194+yan-trindade@users.noreply.github.com>
2024-11-24 13:15:03 -0300

    Update set.h

--- projeto2/set.h
+++ projeto2/set.h
@@ -4,17 +4,27 @@
     #include<stdbool.h>
     #include<stdlib.h>
     #include "avl.h"
+    #include "rb.h"
 
 
     typedef struct set_ SET;
 
-    SET *set_criar(void);
-    void set_apagar(SET **set);
-    bool set_inserir(SET *set, int chave);
-    bool set_remover(SET *set, int chave);
-    bool set_pertence(SET *set, int chave);
-    SET *set_uniao(SET *A, SET *B);
-    SET *set_intersecao(SET *A, SET *B);
-    void set_imprimir(SET *set);
+    SET *set_avl_criar(void);
+    void set_avl_apagar(SET **set);
+    bool set_avl_inserir(SET *set, int chave);
+    bool set_avl_remover(SET *set, int chave);
+    bool set_avl_pertence(SET *set, int chave);
+    SET *set_avl_uniao(SET *A, SET *B);
+    SET *set_avl_intersecao(SET *A, SET *B);
+    void set_avl_imprimir(SET *set);
+
+    SET *set_rb_criar(void);
+    void set_rb_apagar(SET **set);
+    bool set_rb_inserir(SET *set, int chave);
+    bool set_rb_remover(SET *set, int chave);
+    bool set_rb_pertence(SET *set, int chave);
+    SET *set_rb_uniao(SET *A, SET *B);
+    SET *set_rb_intersecao(SET *A, SET *B);
+    void set_rb_imprimir(SET *set);
 
 #endif
