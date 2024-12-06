60ce61c1228fcffd4e15bdf237c6d4c79ca5b7f9
raí fernando <raifernandodalpra@gmail.com>
2024-12-06 14:17:24 -0300

    doc: main e set.h

--- projeto2/main.c
+++ projeto2/main.c
@@ -1,8 +1,43 @@
+/*
+ * PROJETO 2 - ALGORITMOS E ESTRUTURAS DE DADOS I
+ * 
+ * Este projeto tem como objetivo implementar um Tipo Abstrato de Dado (TAD) 
+ * que representa um conjunto, oferecendo operações como inserção, remoção, 
+ * interseção e união de dois conjuntos.
+ * 
+ * O cliente pode escolher entre duas diferentes estruturas de dados para 
+ * implementar essas funcionalidades: árvores binárias balanceadas do tipo 
+ * AVL ou RUBRO-NEGRA. 
+ * 
+ * Essas estruturas foram escolhidas devido à sua eficiência na organização 
+ * dos elementos, facilitando a verificação de pertencimento de um elemento 
+ * e permitindo a recuperação dos elementos de forma ordenada. Essa 
+ * ordenação simplifica as operações de união e interseção, tornando-as mais 
+ * rápidas e eficientes.
+ * 
+ * Alunos (grupo 35):
+ * - Raí Fernando - 15506968 - raifernando@usp.br 
+ * - Yan Trindade Meireles - 13680035 - yan.trindade@usp.br
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "set.h"
 
+/*
+Na função main(), são lidos da entrada padrão, na ordem, um valor entre 0 e 1, 
+indicando a estrutura escolhida (AVL ou Rubro-Negra), e os tamanhos dos 
+conjuntos A e B. Com base nessas informações, os  conjuntos são criados, e os elementos 
+de A e B são inseridos nos respectivos conjuntos.
+
+Em seguida, é lido um inteiro OPERAÇÃO (entre 1 e 4), que determina qual operação será realizada 
+sobre os conjuntos: (1) verificar se um elemento x pertence ao conjunto A; (2) realizar a união entre 
+A e B; (3) calcular a interseção entre A e B; (4) ???????????. 
+A operação correspondente é então executada utilizando um bloco switch/case.
+
+Por fim, a memória alocada pelos conjuntos é devidamente liberada.
+*/
 int main(){
     int estrutura_escolhida, tam_set_a, tam_set_b;
     scanf("%d", &estrutura_escolhida);
@@ -42,7 +77,7 @@ int main(){
         }
 
         case 2: {
-                //União do Conjunto A com B
+                // União do Conjunto A com B
                 SET *C = set_uniao(A, B);
                 set_imprimir(C);                
                 break;
@@ -56,7 +91,7 @@ int main(){
         }
         
         case 4: {
-                //Remoção ------- em a ou b????
+                // Remoção ------- em a ou b????
                 SET *C;
                 set_imprimir(C);
         }
--- projeto2/set.h
+++ projeto2/set.h
@@ -7,13 +7,35 @@
 
     typedef struct set_ SET;
 
+    /* Cria um novo conjunto com a estrutura interna escolhida pelo parâmetro `estrutura_escolhida`. 
+    * O valor 0 representa uma Árvore Binária AVL, e o valor 1 representa uma Árvore Binária Rubro-Negra.
+    * Retorna um ponteiro para o conjunto criado ou NULL em caso de falha. */
     SET *set_criar(int estrutura_escolhida);
+
+    /* Libera a memória alocada para o conjunto. 
+    * O parâmetro deve ser o endereço do ponteiro para o conjunto, permitindo liberar corretamente a memória.
+    * Não possui retorno. */
     void set_apagar(SET **set);
+
+    /* Insere a chave especificada no conjunto. 
+    * Retorna `true` se a inserção foi realizada com sucesso; caso contrário, retorna `false`.*/
     bool set_inserir(SET *set, int chave);
+
+    /* Remove a chave do conjunto. 
+    * Retorna `true` se a remoção foi realizada com sucesso; caso contrário, retorna `false`. */
     bool set_remover(SET *set, int chave);
+    
+    /* Verifica se a chave especificada pertence ao conjunto. 
+    * Retorna `true` se a chave está presente; caso contrário, retorna `false`. */
     bool set_pertence(SET *set, int chave);
+
+    /* Cria e retorna um novo conjunto representando o união de A e B */
     SET *set_uniao(SET *A, SET *B);
+
+    /* Cria um novo conjunto representando a intersecção entre A e B */
     SET *set_intersecao(SET *A, SET *B);
+
+    /* Imprime todos os elementos do conjunto, seguindo o percurso em-ordem */
     void set_imprimir(SET *set);
 
 #endif
